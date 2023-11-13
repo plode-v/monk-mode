ac07475e1e326414ab14b445e2c9d1480cabb46e
Plode <84858705+plode-v@users.noreply.github.com>
2023-11-13 19:37:54 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Platform } from 'react-native';
+import { useState } from 'react';
+import Homepage from './Homepage';
+import { Navigation } from 'react-native-navigation';
 
 export default function App() {
+
+  const [on, setOn] = useState(false);
+
+  const handleClick = () => {
+    setOn(!on);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={on ? styles.pressOne : styles.pressTwo} onPress={handleClick} ref={Homepage}>
+        <Text style={styles.text}>This is homepage</Text>
+      </Text>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    height: '100%',
+    backgroundColor: "#FAFAFA",
+    display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: 'white'
+  },
+  pressOne: {
+    backgroundColor: 'red'
+  },
+  pressTwo: {
+    backgroundColor: 'blue',
+  }
 });
+
+Navigation.registerComponent("Homepage", () => Homepage);
+
+Navigation.events().registerAppLaunchedListener(async () => {
+  Navigation.setRoot({
+    root: {
+      stack: {
+        children: [
+          {
+            component: {
+              name: 'Home'
+            }
+          }
+        ]
+      }
+    }
+  })
+})
